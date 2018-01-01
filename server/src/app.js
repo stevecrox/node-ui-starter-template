3d428ad02b6111a048209b7ecf2feafc50b41409
Stephen Crocker <stevecrox@tablet.uxbridge-ln.co.uk>
2018-01-01 19:58:08 +0000

    Updated to use express 4 libraries

--- server/src/app.js
+++ server/src/app.js
@@ -1,38 +1,63 @@
-
 /**
- * Module dependencies.
+ * Express Modules
  */
-
 var express = require('express');
-var http = require('http');
+var bodyParser = require('body-parser');
+var errorhandler = require('errorhandler');
+var favicon = require('serve-favicon');
+var morgan = require('morgan');
+var serveStatic = require('serve-static');
+
+var fs = require('fs');
 var path = require('path');
 
-var routes = require('./routes');
+/**
+ * Define all Files which hold handling for REST calls.
+ */
+var routes = require('./routes/index');
 var user = require('./routes/user');
 
-var app = express();
+//Local configuration files used to dynamically configure the project
+var appConfig = require(path.join(__dirname, '../', '../', 'config', 'config.js'));
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.use(express.favicon());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+// start building the web server
+var app = express();
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 // Path to the client folder.
-app.use(express.static(path.join(__dirname, '../', '../', 'client', 'src')));
+var publicPath = path.join(__dirname, '../', '../', 'client', 'src');
+app.use(serveStatic(publicPath));
+
+// Favicon setup can be in config or we can try a default path
+var faviconPath;
+if ('favicon' in appConfig) {
+  faviconPath = appConfig.faviconPath;
+} else {
+  faviconPath = path.join(publicPath, appConfig.contextRoot, 'ico', 'favicon.ico');
+}
+// Only setup Favicon  if we have a valid file
+if (fs.existsSync(faviconPath)) {
+	app.use(favicon(faviconPath));
+}
 
 // development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
-  app.use(express.logger('dev'));
-} else {
-	app.use(express.logger('dev'));
+if ('development' === appConfig.environment) {
+  app.use(errorhandler());
+  app.use(morgan('combined'))
 }
+
+
+/**
+ * This is where we define
+ */
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+// Start the server
+app.listen(appConfig.port, function() {
+  console.log('Express server listening on port ' + appConfig.port);
 });
