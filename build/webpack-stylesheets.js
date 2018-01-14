49542f1cb6cadfd5ce5f25c6ec1f64b5baeabca4
Stephen Crocker <stevecrox@tablet.uxbridge-ln.co.uk>
2018-01-14 22:48:56 +0000

    Anouther update, darn webpacks!

--- build/webpack-stylesheets.js
+++ build/webpack-stylesheets.js
@@ -2,6 +2,8 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
+//'font-awesome': [ 'font-awesome/css/font-awesome.css' ]
+
 // Local configuration files used to dynamically configure the project
 var appConfig = require(path.join(__dirname, '../', 'config', 'config.js'));
 
@@ -10,13 +12,20 @@ const extractLESS = new ExtractTextPlugin('[name].less');
 
 module.exports = {
 	entry: {
-	    datatables: [ 'datatables.net-bs/css/dataTables.bootstrap.css' ]
+	    datatables: [ 'datatables.net-bs/css/dataTables.bootstrap.css' ],
+	    bootstrap: [ "bootstrap-webpack"]
 	},
 	output : {
 		path : path.join(appConfig.clientDirectory, 'stylesheets'),
 		filename : '[name].css'
 	},
 	module : {
+	    loaders: [
+	              { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=application/font-woff" },
+	              { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=application/octet-stream" },
+	              { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+	              { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=image/svg+xml" }
+	            ],
 		rules : [
 		{
 			test: /\.css$/,
